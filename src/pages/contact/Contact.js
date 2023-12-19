bcf19d89ed5757e8f62221fc4d465ec11a059beb
Jakob <j.wilmesmeier@gmail.com>
2023-12-19 10:47:50 +0100

    fix visibilities

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
 				<h1 className="text-headline text-2xl font-bold">Kontakt</h1>
 			</div>
 			<p>
-				Anfragen bitte an{" "}
+				Anfragen, Anregungen und Feedback bitte an{" "}
 				<a
 					className="text-href font-bold"
 					href="mailto:spielemanufaktur@outlook.com"
--- src/pages/privacy/Privacy.js
+++ src/pages/privacy/Privacy.js
@@ -2,13 +2,15 @@ import privacyPdf from "../../assets/privacy.pdf";
 
 export default function Privacy() {
 	return (
-		<div className="w-full text-start">
-			<h1 className="text-headline text-2xl font-bold mb-4">
-				Datenschutzerklärung
-			</h1>
-			<a className="text-href" href={privacyPdf} download>
+		<>
+			<div className="w-full text-center">
+				<h1 className="text-headline text-2xl font-bold mb-4">
+					Datenschutzerklärung
+				</h1>
+			</div>
+			<a className="text-href text-start" href={privacyPdf} download>
 				Klicken zum Herunterladen der Datenschutzerklärung
 			</a>
-		</div>
+		</>
 	);
 }
